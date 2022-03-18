a6a8106ec295db5d1b32d364f257688da5d4b266
Robyarta Haruli Ruci <kugelfang.killaruna@gmail.com>
2022-03-18 09:19:48 +0700

    add translucent multiple button horizontal

---
+++ lib/infrastructure/auth/widgets/translucent_multiple_button_horizontal.widget.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class TranslucentMultipleButtonHorizontal extends StatelessWidget {
+
+  final Size? size;
+  final String? leftText;
+  final String? rightText;
+  final VoidCallback? onTapLeftText;
+  final VoidCallback? onTapRightText;
+
+  const TranslucentMultipleButtonHorizontal({
+    Key? key,
+    this.leftText,
+    this.rightText,
+    this.onTapLeftText,
+    this.onTapRightText,
+    this.size
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: size?.height,
+      width: size?.width,
+      child: Center(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            RichText(
+              text: TextSpan(
+                text: leftText,
+                style: const TextStyle(color: Colors.white),
+                recognizer: TapGestureRecognizer()..onTap = () {
+                    HapticFeedback.lightImpact();
+                    onTapLeftText!();
+                  },
+              ),
+            ),
+            RichText(
+              text: TextSpan(
+                text: rightText,
+                style: const TextStyle(color: Colors.white),
+                recognizer: TapGestureRecognizer()..onTap = () {
+                    HapticFeedback.lightImpact();
+                    onTapRightText!();
+                  },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+}

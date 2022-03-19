c9dfe7b99fcb1dc47c2c99a416c0cd7b209bb5dd
Robyarta Haruli Ruci <kugelfang.killaruna@gmail.com>
2022-03-19 15:27:47 +0700

    add basic update profile

--- lib/app/screens/profile.screen.dart
+++ lib/app/screens/profile.screen.dart
@@ -1,7 +1,6 @@
 import 'package:buka_trip/infrastructure/util/constant.dart';
 import 'package:buka_trip/infrastructure/util/dialog_presenter.dart';
 import 'package:buka_trip/infrastructure/util/log.dart';
-import 'package:buka_trip/infrastructure/util/validator.dart';
 import 'package:buka_trip/infrastructure/widgets/translucent_multiple_button_horizontal.widget.dart';
 import 'package:buka_trip/infrastructure/widgets/translucent_text_field.widget.dart';
 import 'package:firebase_auth/firebase_auth.dart';
